df999792bc71bf4ea3723cec3d04202b5c13d1a2
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2022-01-16 18:12:57 -0400

    reworks in logic

--- js.js
+++ js.js
@@ -1,35 +1,30 @@
-let scoreKeeper = 0;
 
-window.alert("Welcome to Rock, Paper, Scissor. Open your dev tools console and re-load the website to see the game results.");
+var pointPlayer = 0;
+var pointPc = 0;
+
+window.alert("Welcome to Rock, Paper, Scissors. Open your dev tools console and re-load the website to see the game results.");
 
 function randomChoice(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-
 function computerPlay() {
     let chooser = randomChoice(1, 4);
-
     if (chooser === 3) {
         let pcChoice = "PAPER";
         console.log("Computer chose "+ pcChoice +"!");
         return pcChoice;
     }
-
     else if (chooser === 2) {
         let pcChoice = "ROCK";
         console.log("Computer chose "+ pcChoice +"!");
         return pcChoice;  
     }
-
-    else {
-        
+    else { 
         let pcChoice = "SCISSOR";
         console.log("Computer chose "+ pcChoice +"!");
         return pcChoice;
     }
-
-    
 }
 
 function playerPlay() {
@@ -38,46 +33,36 @@ function playerPlay() {
     return playerChoice;
 }
 
-
+    
 
 function singleRound(player, computer) {
 
-    let point = undefined;
-
     if (player.toUpperCase() == computer) {
         console.log("This round was a draw!");
-        point = 0.5;
-        return point;
+        pointPlayer += 0.5;
+        pointPc += 0.5;
+        
     }
 
     else {
         if (player.toUpperCase() == "ROCK" && computer == "PAPER"){
             console.log("This round the human lost!");
-            point = 0;
-            return point;
+            pointPc += 1;
         }
-
         else if (player.toUpperCase() == "PAPER" && computer == "SCISSOR"){
             console.log("This round the human lost!");
-            point = 0;
-            return point;
+            pointPc += 1;
         }
-
         else if (player.toUpperCase() == "SCISSOR" && computer == "ROCK"){
             console.log("This round the human lost!");
-            point = 0;
-            return point;
+            pointPc += 1;
         }
-
-
         else {
             console.log("This round the human won!");
-            point = 1;
-            return point;
-        }
+            pointPlayer += 1;
 
-    }
-    
+        }
+    }   
 }
 
 
@@ -93,30 +78,31 @@ for(let i = 0; i <= 4; i++){
 
     let scorePc = computerPlay();
 
-    let roundResult = singleRound(scorePl, scorePc);
+    singleRound(scorePl, scorePc);
 
-    console.log(roundResult);
-
-    scoreKeeper = scoreKeeper + roundResult;   
+    console.log(pointPlayer + " is the player's score" );
+    
+    if (pointPlayer >= 3) break;
 }
 
 
+function didIWin(){
 
-
-function didIWin(results){
-    let scoreCheck = 0;
-    scoreCheck = results;
-    if (results > 2.5){
-        console.log("You win the match! Final score is: " + scoreCheck+ "/5. Congratulations!");
+    if (pointPlayer > pointPc){
+        console.log("You win the match! Final score is: " + pointPlayer+ "/5. Congratulations!");
+        return true;
     }
 
-    else if (results == 2.5){
-        console.log("You tie the match! Final score is: " + scoreCheck+ "/5. Formidable opponent!");
+    else if (pointPlayer == pointPc){
+        console.log("You tie the match! Final score is: " + pointPlayer+ "/5. Formidable opponent!");
+        return true;
     }
 
     else {
-        console.log("You lose! Final score is: " + scoreCheck+ "/5. Dirty, sad, ugly loser!");
+        console.log("You lose! Final score is: " + pointPlayer+ "/5. Dirty, sad, ugly loser!");
+        return false;
     }
 }
 
-didIWin(scoreKeeper);
+didIWin();
+
